460427c1ddefdddafafc93c7fcb8da3ac27fa7cd
Umer Inayat <umerinayat.369@gmail.com>
2021-07-04 23:03:05 +0500

    added client auth

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'happy-valley-plants.local,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
+        'happy-valley-plants.local,hvp.umerinayat.com,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
     )),
 
     /*
